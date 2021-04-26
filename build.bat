c0c6217c170d55abe5a5f8ea41171ded1cc648c0
a <ya.mikushoff@gmail.com>
2021-04-27 00:55:07 +0300

    fix

--- build.bat
+++ build.bat
@@ -1,7 +1,9 @@
 @echo off
 cd backend-api && start gralew build && cd ../
-cd job-logger && start gralew build && cd ../
-cd web-client && start npm run-script build && cd ../ 
+cd text-processing-limiter && start gradlew build && cd ../
 cd text-rank-calc && start gradlew build && cd ../
 cd vowel-cons-counter && start gradlew build && cd ../
 cd vowel-cons-rater && start gradlew build && cd ../
+cd text-statistics && start gradlew build && cd ../
+cd job-logger && start gralew build && cd ../
+cd frontend && start npm run-script build && cd ../ 
--- run.bat
+++ run.bat
@@ -1,7 +1,9 @@
 @echo off
 cd backend-api && start run.bat && cd ../
-cd job-logger && start run.bat && cd ../
-cd web-client && start npm start && cd ../ 
+cd text-processing-limiter && start run.bat && cd ../
 cd text-rank-calc && start run.bat && cd ../
+cd job-logger && start run.bat && cd ../
 cd vowel-cons-counter && start run.bat && cd ../
 cd vowel-cons-rater && start run.bat && cd ../
+cd text-statistics && start run.bat && cd ../
+cd frontend && start npm start && cd ../ 
